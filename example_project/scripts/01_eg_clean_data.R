82c5234741a0716bf040cd8e1c7e6883e43d46d5
Brendan Palmer <30694114+bapalmer@users.noreply.github.com>
2019-04-27 21:18:27 +0100

    Update 01_eg_clean_data.R

--- example_project/scripts/01_eg_clean_data.R
+++ example_project/scripts/01_eg_clean_data.R
@@ -1,7 +1,7 @@
 #########################################################################
 # 1-day R workshop 
 # Morning practical session B
-# 9th February 2019
+# 3rd May 2019
 # 03_eg_clean_data.R
 #########################################################################
 # Load the required packages
