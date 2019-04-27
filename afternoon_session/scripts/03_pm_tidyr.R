fb38065bd5a1a48ff9cdc5726b555e9d6e5ae47c
Brendan Palmer <30694114+bapalmer@users.noreply.github.com>
2019-04-27 21:16:48 +0100

    Update 03_pm_tidyr.R

--- afternoon_session/scripts/03_pm_tidyr.R
+++ afternoon_session/scripts/03_pm_tidyr.R
@@ -1,7 +1,7 @@
 #########################################################################
 # 1-day R workshop 
 # Afternoon practical session A
-# 9th February 2019
+# 3rd May 2019
 # 03_pm_tidyr.R
 #########################################################################
 # tidyr is loaded when the tidyverse package is called
